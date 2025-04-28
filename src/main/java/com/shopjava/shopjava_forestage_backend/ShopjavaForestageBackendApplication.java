6249bb60cf50350a44346e63c5205cc2d5196f26
aass2346789 <aass2346789@gmail.com>
2025-04-28 15:41:10 +0800

    add spring

---
+++ src/main/java/com/shopjava/shopjava_forestage_backend/ShopjavaForestageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shopjava.shopjava_forestage_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopjavaForestageBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopjavaForestageBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopjava/shopjava_forestage_backend/ShopjavaForestageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopjava.shopjava_forestage_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopjavaForestageBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
